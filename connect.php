d1dd0eb3a3f0ea4f1897786bcafcff1fc840ac62
rocktohq <itzmonir@gmail.com>
2024-09-01 19:36:14 +0600

    Some minor updates

--- connect.php
+++ connect.php
@@ -1,23 +1,27 @@
-<?php
-error_reporting(0);
-/*
-* Application Developed by Saidul Mursalin
-* Database Connection Configuration
+/*  
+  # Library Management System
+	#	Application by Saidul Mursalin
+	#	Design & Developed by Saidul Mursalin
+	#	Contact: facebook/itzmonir
 */
 
-// DB Information
-$hostName = 'localhost';    // Host Name
-$dbName = 'students';       // Database Name
-$dbUser = 'students';       // Database Username
-$dbPass = 'students321';    // Database Password
+# Database Configuration
+// Database Information
+$host = 'localhost';        // Host Name
+$dbName = 'library';       // Database Name
+$dbUser = 'library';       // Database Username
+$dbPass = 'library321***';    // Database Password
 
 // Let's try to Connect
-$connect = new mysqli($hostName, $dbUser, $dbPass, $dbName);
+$connect = new mysqli($host, $dbUser, $dbPass, $dbName);
 
 // If there is any Error
-if($connect->connect_errno) {
-  echo "<strong>Failed to connect to MySQL:</strong> " . $connect->connect_error;
+if($connect -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $connect -> connect_error;
+
+  // Storing the Error Log for Future Investigation
+		$errordate = date("F j, Y, g:i a");
+		$message = "{$con -> connect_error} | {$errordate} \r\n";
+		file_put_contents("error-log.txt", $message, FILE_APPEND);
   exit();
 }
-
-?>
